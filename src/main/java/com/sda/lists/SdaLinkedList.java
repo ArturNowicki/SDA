2e614143519ac1784d6b85a379d7a2b818215f2c
Artur Nowicki <arturnowicki@gmail.com>
2016-10-05 14:34:50 +0200

    working
    added removeAll(value)

--- src/main/java/com/sda/lists/SdaLinkedList.java
+++ src/main/java/com/sda/lists/SdaLinkedList.java
@@ -2,66 +2,103 @@ package com.sda.lists;
 
 public class SdaLinkedList {
 
-    private SdaLinkedNode head, lastNode;
+	private SdaLinkedNode head, lastNode;
 
-    public SdaLinkedList(String value) {
-        this.head = new SdaLinkedNode(value);
-        lastNode = head;
-    }
+	public SdaLinkedList(String value) {
+		this.head = new SdaLinkedNode(value);
+		lastNode = head;
+	}
 
-    public void add(String value) {
-    	SdaLinkedNode newNode = new SdaLinkedNode(value);
-    	if(head == null) {
-    		head = newNode;
-    		lastNode = head;
-    	} else {
-    		lastNode.next = newNode;
-        	lastNode = newNode;
-    	}
-    }
+	public void add(String value) {
+		SdaLinkedNode newNode = new SdaLinkedNode(value);
+		if (head == null) {
+			head = newNode;
+			lastNode = head;
+		} else {
+			lastNode.next = newNode;
+			lastNode = newNode;
+		}
+	}
 
-    public String get(int index) {
-        if(index == 0) {
-        	return head.value;
-        } else {
-        	SdaLinkedNode current = head;
-        	int indexCounter = 0;
-        	while(current.next != null || indexCounter!=index) {
-        		current = current.next;
-        		indexCounter++;
-        	}
-//        	current
-        	return "";
-        }
-    }
+	public String get(int index) throws IndexOutOfBoundsException {
+		String returnedValue;
+		SdaLinkedNode current;
+		if (index < 0 || index >= size())
+			throw new IndexOutOfBoundsException("Tried to get non existent elemeent");
+		if (index == 0) {
+			returnedValue = head.value;
+		} else {
+			current = head;
+			int indexCounter = 0;
+			while (current.next != null && indexCounter != index) {
+				current = current.next;
+				indexCounter++;
+			}
+			returnedValue = current.value;
+		}
+		return returnedValue;
+	}
 
-    public void remove(String value) {
-    	int size = 0;
-    	SdaLinkedNode tmpNode = head;
-    	while(tmpNode.next != null) {
-    		
-    	}
-    	
-    }
-    
-    public int size() {
-    	int size = 0;
-    	SdaLinkedNode tmpNode = head;
-    	while(tmpNode.next != null) {
-    		size++;
-    		tmpNode = tmpNode.next;
-    	}
-        return size+1;
-    }
+	public void removeAll(String value) {
+		SdaLinkedNode tmpNode = head;
+		while (tmpNode.next != null) {
+			if (tmpNode.next.value.equals(value)) {
+				if (tmpNode.next.next != null) {
+					tmpNode.next = tmpNode.next.next;
+				} else {
+					tmpNode.next = null;
+				}
+			} else {
+				tmpNode = tmpNode.next;
+			}
+		}
+		if (head.value.equals(value)) {
+			head = head.next;
+		}
+	}
 
-    private static class SdaLinkedNode {
+	public boolean remove(String value) {
+		SdaLinkedNode tmpNode = head;
+		boolean isRemoved = false;
+		if (head.value.equals(value)) {
+			head = head.next;
+			isRemoved = true;
+		} else {
+			while (tmpNode.next != null && !isRemoved) {
+				if (tmpNode.next.value.equals(value)) {
+					isRemoved = true;
+					if (tmpNode.next.next != null) {
+						tmpNode.next = tmpNode.next.next;
+					} else {
+						tmpNode.next = null;
+					}
+				} else {
+					tmpNode = tmpNode.next;
+				}
+			}
+		}
+		return isRemoved;
+	}
 
-        private String value;
-        private SdaLinkedNode next;
+	public int size() {
+		int size = 1;
+		SdaLinkedNode tmpNode = head;
+		while (tmpNode.next != null) {
+			size++;
+			tmpNode = tmpNode.next;
+		}
+		return size;
+	}
 
-        public SdaLinkedNode(String node) {
-            this.value = node;
-        }
-    }
+	private static class SdaLinkedNode {
+
+		private String value;
+		private SdaLinkedNode next;
+
+		public SdaLinkedNode(String value) {
+			next = null;
+			this.value = value;
+		}
+	}
 
 }
--- src/test/java/com/sda/lists/SdaLinkedListTest.java
+++ src/test/java/com/sda/lists/SdaLinkedListTest.java
@@ -5,36 +5,69 @@ import org.junit.Test;
 
 public class SdaLinkedListTest {
 
-    @Test
-    /**
-     * Dobrym sposobem na zrozumienie czegos - jest napisanie tego.
-     * My stworzymy bardzo prosta imlpementacje Single Linked List.
-     */
-    public void linkedList() {
-        SdaLinkedList list = new SdaLinkedList("jeden");
-        list.add("dwa");
-        list.add("trzy");
+	@Test
+	/**
+	 * Dobrym sposobem na zrozumienie czegos - jest napisanie tego. My stworzymy
+	 * bardzo prosta imlpementacje Single Linked List.
+	 */
+	public void linkedList() {
+		SdaLinkedList list = new SdaLinkedList("jeden");
+		list.add("dwa");
+		list.add("trzy");
+		Assert.assertEquals(list.size(), 3);
+		Assert.assertEquals(list.get(0), "jeden");
+		list.remove("jeden");
+		Assert.assertEquals(list.size(), 2);
+		Assert.assertEquals(list.get(0), "dwa");
+		list.add("cztery");
+		list.remove("trzy");
+		Assert.assertEquals(list.size(), 2);
+		Assert.assertEquals(list.get(0), "dwa");
+		Assert.assertEquals(list.get(1), "cztery");
 
-        Assert.assertEquals(list.size(), 3);
-        Assert.assertEquals(list.get(0), "jeden");
-        list.remove("jeden");
-        Assert.assertEquals(list.size(), 2);
-        Assert.assertEquals(list.get(0), "dwa");
-        list.add("cztery");
-        list.remove("trzy");
-        Assert.assertEquals(list.size(), 2);
-        Assert.assertEquals(list.get(0), "dwa");
-        Assert.assertEquals(list.get(1), "cztery");
+		try {
+			list.get(Integer.MAX_VALUE);
+		} catch (IndexOutOfBoundsException exception) {
+			System.out.println("yes1");
+		}
+		try {
+			list.get(-1);
+		} catch (IndexOutOfBoundsException exception) {
+			System.out.println("yes2");
+		}
+	}
 
-        try {
-            list.get(Integer.MAX_VALUE);
-        } catch (IndexOutOfBoundsException exception) {
-            // Sukces!
-        }
-        try {
-            list.get(-1);
-        } catch (IndexOutOfBoundsException exception) {
-            // Sukces!
-        }
-    }
+	@Test
+	public void linkedListMyTest() {
+		SdaLinkedList list = new SdaLinkedList("jeden");
+		list.add("dwa");
+		list.add("trzy");
+		list.add("jeden");
+		list.add("trzy");
+
+		Assert.assertEquals(5, list.size());
+		list.remove("trzy");
+		Assert.assertEquals(4, list.size());
+		Assert.assertEquals("jeden", list.get(2));
+	}
+
+	@Test
+	public void removeAllTest() {
+		SdaLinkedList list = new SdaLinkedList("jeden");
+		list.add("jeden");
+		list.add("dwa");
+		list.add("trzy");
+		list.add("trzy");
+		list.add("cztery");
+		list.add("cztery");
+		Assert.assertEquals(7, list.size());
+
+		list.removeAll("cztery");
+		Assert.assertEquals(5, list.size());
+
+		list.removeAll("jeden");
+		Assert.assertEquals("dwa", list.get(0));
+		Assert.assertEquals(3, list.size());
+
+	}
 }
